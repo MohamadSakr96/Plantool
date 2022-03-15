ede0abe9170405a4ae3d813df5768aa3ac812861
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-15 21:33:23 +0200

    add: routes for different pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,25 @@
-
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './pages/login';
+import { Planning } from './pages/planning';
+import { Profile } from './pages/profile';
+import { Projects } from './pages/projects';
+import { Register } from './pages/register';
+import { Stats } from './pages/stats';
+import { Team } from './pages/team';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Routes>
+        <Route path='register' element={ <Register /> } />
+        <Route path='login' element={ <Login /> } />
+
+        <Route path='/' element={ <Planning /> } />
+        <Route path='team' element={ <Team /> } />
+        <Route path='projects' element={ <Projects /> } />
+        <Route path='stats' element={ <Stats /> } />
+        <Route path='profile' element={ <Profile /> } />
+      </Routes>
     </div>
   );
 }
