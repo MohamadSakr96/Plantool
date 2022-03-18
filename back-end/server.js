e3daad85b65c2e102d72dd8a538e06a5d3bc13cd
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-18 19:19:33 +0200

    add: users and roles module. Plus initialize roles in database

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const dbConfig = require("./app/config/db.config");
+const db = require("./app/models");
+const Role = require("./app/models/role.model");
 
+const app = express();
 var corsOptions = {
     origin: "http://localhost:8081"
 };
@@ -10,6 +13,53 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// connection to plantool database
+db.mongoose
+    .connect(
+        `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
+    )
+    .then(() => {
+        console.log("Successfully connected to database.");
+        inital();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+// inital run, if the database is empty, we create important rows in roles collection 
+
+function inital() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("add 'user' to roles collection");
+            });
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("add 'admin' to roles collection");
+            });
+            new Role({
+                name: "pending"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("add 'pending' to roles collection");
+            });
+        }
+    });
+}
 
 // simple route
 app.get("/", (req, res) => {
