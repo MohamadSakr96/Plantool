bd87d9f463f799f553ccf8294cd516229caff48f
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-18 17:52:04 +0200

    add: simple route and listen to test server

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,23 @@
-console.log("Hello from server")
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+// parse requests
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my app." });
+});
+
+// set port to listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
