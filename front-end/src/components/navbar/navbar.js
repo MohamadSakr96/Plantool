cf6ea75cb996cbedc03cfbcbd0a40259a6214e04
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-21 00:06:57 +0200

    add: profile html structure

--- front-end/src/components/navbar/navbar.js
+++ front-end/src/components/navbar/navbar.js
@@ -6,19 +6,19 @@ import default_profile_pic from "../../assets/default_profile_icon.png";
 export const Navbar = () => {
   return (
     <div className='container-navbar'>
-        <div className='container-logo'>
+        <div className='container-navbar_logo'>
             <Link to='planning'>Plantool</Link>
         </div>
-        <div className='container-menu'>
+        <div className='container-navbar_menu'>
             <Link to='planning'>Planning</Link>
             <Link to='team'>Team</Link>
             <Link to='projects'>Projects</Link>
             <Link to='stats'>Stats</Link>
         </div>
         <Link to='profile'>
-          <div className='container-profile'>
-            <div className='profile-name'>Mohamad Sakr</div>
-            <div className='profile-picture'>
+          <div className='container-navbar_profile'>
+            <div className='container-navbar_profile-name'>Mohamad Sakr</div>
+            <div className='container-navbar_profile-picture'>
               <img src={default_profile_pic} alt="profile pic"/>
             </div>
           </div>
--- front-end/src/pages/profile.js
+++ front-end/src/pages/profile.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 export const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='container-profile'>
+      <div className='container-profile_title'>
+        <h1>Profile</h1>
+      </div>
+      <div className='container-profile_content'>
+        <div className='container-profile_card'></div>
+        <div className='container-profile_information'>
+          <div className='basic-information'></div>
+          <div className='security'></div>
+        </div>
+      </div>
+    </div>
   )
 }
