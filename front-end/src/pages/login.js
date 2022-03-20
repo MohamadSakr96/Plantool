b3b99b4ead5389c643155ee3d3a64bc5e6f921d8
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-20 20:48:10 +0200

    add: login form content

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='container-login'>
+      <div className='container-title'>
+        <h1 className='title'>Plantool</h1>
+      </div>
+      <div className='container-login-form'>
+      <Box component="form" sx={{ mt: 3, mr: 2, ml: 2 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    size="small"
+                />
+                </Grid>
+                <Grid item xs={12}>
+                <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    size="small"
+                />
+                </Grid>
+            </Grid>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 1 }}
+            >
+                login
+            </Button>
+            <Grid container justifyContent="center">
+                <Grid item sx={{mb: 1}}>
+                <Link to={"/"} variant="body2">
+                  Don't have an account? Register
+                </Link>
+                </Grid>
+            </Grid>
+            </Box>
+      </div>
+    </div>
   )
 }
