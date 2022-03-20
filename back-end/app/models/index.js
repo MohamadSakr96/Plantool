a353bddc053baa9b4abc9693b5960a2f428a7253
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-20 09:29:59 +0200

    Removed all files that are in the .gitignore

--- back-end/app/models/index.js
+++ back-end/app/models/index.js
@@ -5,4 +5,5 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.user = require("./user.model");
+db.refreshToken = require("./refreshToken.model");
 module.exports = db;
