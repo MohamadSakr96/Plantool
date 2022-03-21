9b886da444a6fedd167322d9ecf281c140eea05e
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-21 19:37:49 +0200

    add: css for profile page

--- front-end/src/pages/profile.js
+++ front-end/src/pages/profile.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import default_profile_pic from "../assets/default_profile_icon.png";
+
 
 export const Profile = () => {
   return (
     <div className='container-profile'>
       <div className='container-profile_title'>
         <h1>Profile</h1>
+        <button type='button'>Save</button>
       </div>
       <div className='container-profile_content'>
-        <div className='container-profile_card'></div>
+        <div className='container-profile_card'>
+          <div className='container-profile_card-header'>
+            <img src={default_profile_pic} alt="profile pic"/>
+          </div>
+          <div className='container-profile_card-body'>
+            <div className='card-profile_name'>Mohamad Sakr</div>
+            <div className='card-profile_email'>MohamadSakr1996@gmail.com</div>
+          </div>
+          <div className='container-profile_card-footer'>
+            <button className='card-profile_name' type='button'>Change Profile Picture</button>
+          </div>
+        </div>
         <div className='container-profile_information'>
-          <div className='basic-information'></div>
-          <div className='security'></div>
+          <div className='basic-information'> basic info</div>
+          <div className='security'>sicurity</div>
         </div>
       </div>
     </div>
