80764ababb9b497d98bb889cb52466afbff561e6
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-23 18:26:21 +0200

    add: redux store/action/reducer for login and logout

---
+++ front-end/src/features/auth/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        value: localStorage.getItem('user'),
+    },
+    reducers: {
+        authenticate: state => {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
+            state.value = localStorage.getItem('user'); 
+        },
+        forget: state => {
+            state.value = null
+        }
+    }
+})
+
+// Action creators are generated for each case reducer function
+export const { authenticate, forget } = authSlice.actions;
+
+export default authSlice.reducer;
