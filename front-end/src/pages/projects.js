a12effa89222174ca622c7bd53bba23cb1746c21
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-25 18:58:44 +0200

    add: title container for stats page

--- front-end/src/pages/projects.js
+++ front-end/src/pages/projects.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import AddIcon from '@mui/icons-material/Add';
+
+// Fake data
+function createData(id, first_name, last_name, entry_date, salary, position, vacation_days) {
+  return { id, first_name, last_name, entry_date, salary, position, vacation_days };
+}
+const rows = [
+  createData( 1, 'Mohamad', 'Sakr', '2/22/2022', 1000, 'Junior', 15),
+  createData( 2, 'Joe', 'Rizk', '2/22/2022', 1100, 'Junior', 10),
+  createData( 3, 'Caline', 'Yammine', '2/22/2022', 1500, 'Junior', 17),
+  createData( 4, 'John', 'Doe', '2/22/2022', 1900, 'Senior', 5),
+  createData( 5, 'Moo', 'Sakr', '2/22/2022', 2000, 'Senior', 20),
+];
 
 export const Projects = () => {
   return (
@@ -7,6 +26,37 @@ export const Projects = () => {
       <div className='container-projects_title'>
         <h1>Projects</h1>
         <AddIcon fontSize='large' color='primary' />
+      </div>
+      <div className='container-projects_content'>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">First Name</TableCell>
+                <TableCell align="center">Last Name</TableCell>
+                <TableCell align="center">Entry Date</TableCell>
+                <TableCell align="center">Salary<br/>(€ / month)</TableCell>
+                <TableCell align="center">Position</TableCell>
+                <TableCell align="center">Days of vacations <br/>remaining</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="center">{row.first_name}</TableCell>
+                  <TableCell align="center">{row.last_name}</TableCell>
+                  <TableCell align="center">{row.entry_date}</TableCell>
+                  <TableCell align="center">{row.salary}</TableCell>
+                  <TableCell align="center">{row.position}</TableCell>
+                  <TableCell align="center">{row.vacation_days}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   )
--- front-end/src/pages/stats.js
+++ front-end/src/pages/stats.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 export const Stats = () => {
   return (
-    <div>Stats</div>
+    <div className='container-stats'>
+      <div className='container-stats_title'>
+        <h1>Stats</h1>
+        <div className='flex'>
+          <div>
+            <div>Start date</div>
+            <input type='date' />
+          </div>
+          <div>
+            <div>End date</div>
+            <input type='date' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
