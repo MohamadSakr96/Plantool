386648d37836eb669e917b63cd2e79f86e2fb428
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-26 21:50:08 +0200

    add: dropdown menu for navbar

--- front-end/src/components/navbar/navbar.js
+++ front-end/src/components/navbar/navbar.js
@@ -3,10 +3,23 @@ import './navbar.css';
 import { Link, Navigate } from "react-router-dom";
 import default_profile_pic from "../../assets/default_profile_icon.png";
 import { useSelector } from 'react-redux';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export const Navbar = () => {
-  let menu;
+  let menu, notification;
   const user = useSelector((state) => state.auth.value);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   
   if (user && user.role !== "pending") {
     if (user.role === "admin") {
@@ -16,6 +29,11 @@ export const Navbar = () => {
             <Link to='team'>Team</Link>
             <Link to='projects'>Projects</Link>
             <Link to='stats'>Stats</Link>
+        </div>
+      );
+      notification = (
+        <div className='container-navbar_profile-notifications'>
+          <NotificationsIcon style={{color: 'white'}} fontSize='small' sx={{ mr: 2, mt: 0.7}}/>
         </div>
       );
     }
@@ -29,14 +47,46 @@ export const Navbar = () => {
             <Link to='/'>Plantool</Link>
         </div>
         {menu}
-        <Link to='profile'>
-          <div className='container-navbar_profile'>
+        <div className='container-navbar_profile'>
+          {notification}
+          <Button 
+          style={{color: 'white', textTransform: 'none'}}
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+          >
             <div className='container-navbar_profile-name'>{user.first_name + " " + user.last_name}</div>
             <div className='container-navbar_profile-picture'>
               <img src={default_profile_pic} alt="profile pic"/>
             </div>
-          </div>
-        </Link>
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem 
+            onClick={handleClose}
+            style={{minWidth:130}} 
+            >
+              <Link to='profile' style={{color:"black"}}>
+                Profile
+              </Link>
+            </MenuItem>
+            <MenuItem 
+            onClick={handleClose}
+            style={{color:"red", minWidth:130}} 
+            >
+              Logout
+            </MenuItem>
+          </Menu>
+        </div>
     </div>
   )
 }
