096bea9a8dfd5f212a17df2223f8ac23a9881c1a
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-27 20:42:14 +0300

    fix: projects model

--- back-end/app/models/project.model.js
+++ back-end/app/models/project.model.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
-const Project = new mongoose.Schema({
-    name: String,
-    type: String,
-    client: String,
-    value: Number,
-    duration: Number
-});
+const Project = mongoose.model(
+    "Project",
+    new mongoose.Schema({
+        name: String,
+        type: String,
+        client: String,
+        value: Number,
+        duration: Number
+    })
+);
+
+
 
 module.exports = Project;
