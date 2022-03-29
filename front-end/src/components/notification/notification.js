2ff752e5121a6dc568bd1b8a62d341184ccda580
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-29 15:11:37 +0300

    add: notification component

---
+++ front-end/src/components/notification/notification.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './notification.css';
+import default_picture from '../../assets/default_profile_icon.png';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+
+export const Notification = () => {
+  return (
+    <div className='container-notification'>
+       <div className='notification-item'>
+           <div className='notification-item_user'>
+                <div className='container-navbar_profile-picture'>
+                    <img src={default_picture} alt="profile pic"/>
+                </div>
+                <div className='container-navbar_profile-name'>Mohamad Sakr</div>
+           </div>
+           <div className='notification-item_action'>
+                <CheckIcon style={{color: 'green'}}/>
+                <CloseIcon style={{color: 'red'}}/>
+           </div>
+       </div>
+    </div>
+  )
+}
