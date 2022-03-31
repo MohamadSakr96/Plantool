d46f47f983a411793e460cad6bacaf84c0d2c3ef
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-31 18:02:37 +0300

    add: render add-team-members button only for admin

--- front-end/src/pages/planning.js
+++ front-end/src/pages/planning.js
@@ -4,47 +4,53 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import { Team_Members } from '../components/team_members/team_members';
 import { Chart } from "react-google-charts";
+import { useSelector } from 'react-redux';
 
 const columns = [
   { type: "string", id: "Employees" },
+  { type: "string", id: "Projects" },
+  // { type: 'string', id: 'style', role: 'style'},
   { type: "datetime", id: "Start" },
   { type: "datetime", id: "End" }
 ];
 
 const rows = [
-  ["Mohamad Sakr", new Date(2020, 3, 30), new Date(2020, 6, 30)],
-  ["Mohamad Sakr", new Date(2020, 6, 30), new Date(2020, 12, 30)],
-  ["Mohamad Sakr", new Date(2021, 1, 30), new Date(2021, 6, 30)],
-  ["Mohamad Sakr", new Date(2021, 6, 30), new Date(2021, 12, 30)],
-  ["Mohamad Sakr", new Date(2022, 1, 30), new Date(2022, 6, 30)],
-  ["Joe Rizk", new Date(2020, 1, 30), new Date(2020, 6, 30)],
-  ["Joe Rizk", new Date(2020, 6, 30), new Date(2020, 12, 30)],
-  ["Joe Rizk", new Date(2021, 3, 30), new Date(2021, 6, 30)],
-  ["Joe Rizk", new Date(2021, 6, 30), new Date(2021, 12, 30)],
-  ["Joe Rizk", new Date(2022, 1, 30), new Date(2022, 6, 30)],
-  ["John Doe", new Date(2020, 2, 30), new Date(2020, 6, 30)],
-  ["John Doe", new Date(2020, 7, 30), new Date(2020, 11, 30)],
-  ["John Doe", new Date(2021, 1, 30), new Date(2021, 2, 30)],
-  ["John Doe", new Date(2021, 6, 30), new Date(2021, 12, 30)],
-  ["John Doe", new Date(2022, 1, 30), new Date(2022, 6, 30)],
-  ["Caline", new Date(2020, 3, 30), new Date(2020, 6, 30)],
-  ["Caline", new Date(2020, 6, 30), new Date(2020, 12, 30)],
-  ["Caline", new Date(2021, 1, 30), new Date(2021, 6, 30)],
-  ["Caline", new Date(2021, 6, 30), new Date(2021, 12, 30)],
-  ["Caline", new Date(2022, 1, 30), new Date(2022, 6, 30)],
-  ["Joe", new Date(2020, 1, 30), new Date(2020, 6, 30)],
-  ["Joe", new Date(2020, 6, 30), new Date(2020, 12, 30)],
-  ["Joe", new Date(2021, 3, 30), new Date(2021, 6, 30)],
-  ["Moo", new Date(2020, 2, 30), new Date(2020, 6, 30)],
-  ["Moo", new Date(2020, 7, 30), new Date(2020, 11, 30)],
-  ["Moo", new Date(2021, 1, 30), new Date(2021, 2, 30)],
-  ["Moo", new Date(2021, 6, 30), new Date(2021, 12, 30)],
-  ["Moo", new Date(2022, 1, 30), new Date(2022, 6, 30)],
+  ["Mohamad Sakr", "Project 1", new Date(2020, 3, 30), new Date(2020, 6, 30)],
+  ["Mohamad Sakr", "Project 2", new Date(2020, 6, 30), new Date(2020, 12, 30)],
+  ["Mohamad Sakr", "Project 3", new Date(2021, 1, 30), new Date(2021, 6, 30)],
+  ["Mohamad Sakr", "Project 4", new Date(2021, 6, 30), new Date(2021, 12, 30)],
+  ["Mohamad Sakr", "Project 5", new Date(2022, 1, 30), new Date(2022, 6, 30)],
+  ["Joe Rizk", "Project 1", new Date(2020, 1, 30), new Date(2020, 6, 30)],
+  ["Joe Rizk", "Project 2", new Date(2020, 6, 30), new Date(2020, 12, 30)],
+  ["Joe Rizk", "Project 3", new Date(2021, 3, 30), new Date(2021, 6, 30)],
+  ["Joe Rizk", "Project 4", new Date(2021, 6, 30), new Date(2021, 12, 30)],
+  ["Joe Rizk", "Project 5", new Date(2022, 1, 30), new Date(2022, 6, 30)],
+  ["John Doe", "Project 1", new Date(2020, 2, 30), new Date(2020, 6, 30)],
+  ["John Doe", "Project 2", new Date(2020, 7, 30), new Date(2020, 11, 30)],
+  ["John Doe", "Project 3", new Date(2021, 1, 30), new Date(2021, 6, 30)],
+  ["John Doe", "Project 4", new Date(2021, 6, 30), new Date(2021, 12, 30)],
+  ["John Doe", "Project 5", new Date(2022, 1, 30), new Date(2022, 6, 30)],
+  ["Caline", "Project 1", new Date(2020, 3, 30), new Date(2020, 6, 30)],
+  ["Caline", "Project 2", new Date(2020, 6, 30), new Date(2020, 12, 30)],
+  ["Caline", "Project 3", new Date(2021, 1, 30), new Date(2021, 6, 30)],
+  ["Caline", "Project 4", new Date(2021, 6, 30), new Date(2021, 12, 30)],
+  ["Caline", "Project 5", new Date(2022, 1, 30), new Date(2022, 6, 30)],
+  ["Joe", "Project 1", new Date(2020, 1, 30), new Date(2020, 6, 30)],
+  ["Joe", "Project 2", new Date(2020, 6, 30), new Date(2020, 12, 30)],
+  ["Joe", "Project 3", new Date(2021, 3, 30), new Date(2021, 6, 30)],
+  ["Moo", "Project 1", new Date(2020, 2, 30), new Date(2020, 6, 30)],
+  ["Moo", "Project 2", new Date(2020, 7, 30), new Date(2020, 11, 30)],
+  ["Moo", "Project 3", new Date(2021, 1, 30), new Date(2021, 6, 30)],
+  ["Moo", "Project 4", new Date(2021, 6, 30), new Date(2021, 12, 30)],
+  ["Moo", "Project 5", new Date(2022, 1, 30), new Date(2022, 6, 30)],
 ];
 const data = [columns, ...rows];
 
 export const Planning = () => {
+  const user = useSelector((state) => state.auth.value);
   const [open, setOpen] = useState(false);
+
+  let renderButton;
 
   const handleOpen = () => {
     setOpen(true);
@@ -53,11 +59,15 @@ export const Planning = () => {
     setOpen(false);
   };
 
+  if (user.role === "admin") {
+    renderButton = <AddIcon onClick={handleOpen} fontSize='large' color='primary'/>;
+  }
+
   return (
     <div className='container-planning'>
       <div className='container-planning_title'>
         <h1>Planning</h1>
-        <AddIcon onClick={handleOpen} fontSize='large' color='primary'/>
+        {renderButton}
         <Dialog
             open={open}
             onClose={handleClose}
@@ -75,7 +85,7 @@ export const Planning = () => {
           data={data}
           width="100%"
           height="100%"
-          
+
         />
       </div>
     </div>
