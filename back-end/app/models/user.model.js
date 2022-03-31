897b47fc961587787e5ef3da01086eb95af7623c
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-03-31 22:54:51 +0300

    add: additional default information on register

--- back-end/app/models/user.model.js
+++ back-end/app/models/user.model.js
@@ -5,10 +5,23 @@ const User = mongoose.model(
     new mongoose.Schema({
         first_name:  String,
         last_name: String,
-        email: String,
+        email: {
+            type: String,
+            required: true,
+            lowercase: true
+        },
+        createdAt: {
+            type: Date,
+            immutable: true,
+            default: () => Date.now(),
+        },
         password: String,
         role: String,
-        image_path: String,
+        image_path: {
+            type: String,
+            default: "http://localhost:8080/images/default_profile_icon.png"
+        },
+
         events: [{
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Event'
