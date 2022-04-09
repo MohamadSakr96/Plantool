c36f434b25d64e6cac6faf9829230e7adab14797
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-04-09 13:19:41 +0300

    add: initial data on clean start (when database is empty)

--- back-end/server.js
+++ back-end/server.js
@@ -4,20 +4,12 @@ const dbConfig = require("./app/config/db.config");
 const db = require("./app/models");
 const User = require("./app/models/user.model");
 var bcrypt = require("bcryptjs");
+const Project = require("./app/models/project.model");
+const Event = require("./app/models/event.model");
+const { initial_users, initial_projects, initial_events } = require("./app/initial_data/index");
 
 const app = express();
-var corsOptions = {
-    origin: '*',
 
-    methods: [
-        'GET',
-        'POST',
-    ],
-
-    allowedHeaders: [
-        'Content-Type'
-    ]
-};
 function setupCORS(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type,Accept,X-Access-Token,X-Key');
@@ -45,29 +37,40 @@ db.mongoose
     )
     .then(() => {
         console.log("Successfully connected to database.");
-        inital();
+        initial();
     })
     .catch(err => {
         console.error("Connection error", err);
         process.exit();
     });
 
-// inital run, if the database is empty, we create important rows in roles collection 
-
-function inital() {
+// inital run, if the database is empty, we create filler data
+function initial() {
     User.estimatedDocumentCount((err, count) => {
         if (!err && count === 0) {
-            new User({
-                first_name: "admin",
-                last_name: "admin",
-                email: "admin@gmail.com",
-                password: bcrypt.hashSync("admin123", 8),
-                role: "admin"
-            }).save(err => {
-                if (err) {
-                    console.log("error", err);
-                }
-                console.log("add 'admin' to users collection");
+            initial_users.map((user)=>{
+                new User(user).save(err => {
+                    if (err) {
+                        console.log("error", err);
+                    }
+                    console.log("Added new user to users collection");
+                });
+            });
+            initial_projects.map((project)=>{
+                new Project(project).save(err => {
+                    if (err) {
+                        console.log("error", err);
+                    }
+                    console.log("Added new project to projects collection");
+                });
+            });
+            initial_events.map((event)=>{
+                new Event(event).save(err => {
+                    if (err) {
+                        console.log("error", err);
+                    }
+                    console.log("Added new event to events collection");
+                });
             });
         }
     });
@@ -78,7 +81,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port to listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
