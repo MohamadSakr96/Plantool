ffae7ed6a06aaaeed9d3cc21bd3c6ced1467703d
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-04-10 23:00:33 +0300

    add: update profile validation checks

--- front-end/src/pages/profile.js
+++ front-end/src/pages/profile.js
@@ -9,11 +9,103 @@ import { useDispatch } from 'react-redux';
 import { authenticate } from '../features/auth/authSlice';
 import { EMP_UPDATE_PROFILE_URL, UPDATE_PROFILE_URL } from '../constants';
 import EditIcon from '@mui/icons-material/Edit';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-
+// import { sha256 } from 'js-sha256';
 
 export const Profile = () => {
   const user = useSelector((state) => state.auth.value);
+  const [input, setInput] = useState({ 
+    first_name: '',
+    last_name: '',
+    current_password: '',
+    new_password: '',
+    password_confirmation: ''
+  });
+  const [error, setError] = useState({
+      first_name: '',
+      last_name: '',
+      email: '',
+      current_password: '',
+      new_password: '',
+      password_confirmation: ''
+  });
+  const handleChange = (event) => {
+      const { name, value } = event.target;
+      setInput(prevState => ({
+          ...prevState,
+          [name]: value
+      }));
+      validateInput(name, value);
+  };
+
+  const validateInput = (name, value) => {
+      if (name === "first_name" || name === "last_name"){
+          if (value.length < 2){
+              setError(prevState => ({
+                  ...prevState,
+                  [name]: "At least 2 letters long"
+              }));
+          }else {
+              setError(prevState => ({
+                  ...prevState,
+                  [name]: ""
+              }));
+          }
+      }
+      if (name === "email") {
+          if (!value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)){
+              setError(prevState => ({
+                  ...prevState,
+                  [name]: "email is not valid!"
+              }));
+          }else {
+              setError(prevState => ({
+                  ...prevState,
+                  [name]: ""
+              }));
+          }
+      }
+      // sha256 is different in the back-end...
+      // if(name === "current_password") {    
+      //   if (sha256(value) === user.password) {
+      //     setError(prevState => ({
+      //       ...prevState,
+      //       [name]: ""
+      //     }));
+      //   }else {
+      //     console.log(sha256(value) );
+      //     setError(prevState => ({
+      //         ...prevState,
+      //         [name]: "wrong password"
+      //     }));
+      //   }
+      // }
+      if (name === "new_password") {
+          if (value.length < 8 || !value.match("^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]+$")) {
+              setError(prevState => ({
+                  ...prevState,
+                  [name]: "invalid password"
+              }));
+          }else {
+              setError(prevState => ({
+                  ...prevState,
+                  [name]: ""
+              }));
+          }
+      }
+      if (name === "password_confirmation") {
+          if (value !== input.password) {
+              setError(prevState => ({
+                  ...prevState,
+                  [name]: "wrong password"
+              }));
+          }else {
+              setError(prevState => ({
+                  ...prevState,
+                  [name]: ""
+              }));
+          }
+      }
+  }
 
   const [image, setImage] = useState('');
   const dispatch = useDispatch();
@@ -46,7 +138,7 @@ export const Profile = () => {
         first_name : object.get("first_name"),
         last_name : object.get("last_name"),
         email : object.get("email"),
-        password: object.get("current_password"),
+        password: object.get("new_password"),
         image_path: image? object.get("image"): '',
       }, {
         headers: {
@@ -121,6 +213,10 @@ export const Profile = () => {
                         name="first_name"
                         autoComplete="first_name"
                         size="small"
+                        onChange={handleChange}
+                        onBlur={handleChange}
+                        error = {Boolean(error?.first_name)}
+                        helperText = {error?.first_name}
                     />
                     </Grid>
                     <Grid item xs={3} alignSelf="center">
@@ -133,6 +229,10 @@ export const Profile = () => {
                         name="last_name"
                         autoComplete="last_name"
                         size="small"
+                        onChange={handleChange}
+                        onBlur={handleChange}
+                        error = {Boolean(error?.last_name)}
+                        helperText = {error?.last_name}
                     />
                     </Grid>
                     <Grid item xs={3} alignSelf="center">
@@ -145,6 +245,10 @@ export const Profile = () => {
                         name="email"
                         autoComplete="email"
                         size="small"
+                        onChange={handleChange}
+                        onBlur={handleChange}
+                        error = {Boolean(error?.email)}
+                        helperText = {error?.email}
                     />
                     </Grid>                 
                   </Grid>
@@ -164,10 +268,15 @@ export const Profile = () => {
                     <Grid item xs={9}>
                     <TextField
                         fullWidth
+                        type="password"
                         label="Current Password"
                         name="current_password"
                         autoComplete="current_password"
                         size="small"
+                        onChange={handleChange}
+                        onBlur={handleChange}
+                        error = {Boolean(error?.current_password)}
+                        helperText = {error?.current_password}
                     />
                     </Grid>
                     <Grid item xs={3} alignSelf="center">
@@ -176,10 +285,15 @@ export const Profile = () => {
                     <Grid item xs={9}>
                     <TextField
                         fullWidth
+                        type="password"
                         label="New Password"
                         name="new_password"
                         autoComplete="new_password"
                         size="small"
+                        onChange={handleChange}
+                        onBlur={handleChange}
+                        error = {Boolean(error?.new_password)}
+                        helperText = {error?.new_password}
                     />
                     </Grid>
                     <Grid item xs={3} alignSelf="center">
@@ -188,10 +302,15 @@ export const Profile = () => {
                     <Grid item xs={9}>
                     <TextField
                         fullWidth
+                        type="password"
                         label="Confirm Password"
                         name="confirm_password"
                         autoComplete="confirm_password"
                         size="small"
+                        onChange={handleChange}
+                        onBlur={handleChange}
+                        error = {Boolean(error?.password_confirmation)}
+                        helperText = {error?.password_confirmation}
                     />
                     </Grid>
                   </Grid>
